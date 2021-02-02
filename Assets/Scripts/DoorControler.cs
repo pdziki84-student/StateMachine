a3e3fb48ebbe901ccca7cfcc0a5569d5f4710220
pdziki84-student <wasza.dzikosc@gmail.com>
2021-02-02 20:37:45 +0100

    StateMachine

---
+++ Assets/Scripts/DoorControler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControler : MonoBehaviour
+{
+    public Transform Door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        Door.GetComponent<Animator>().SetBool("open", true);
+    }
+
+    void OnTriggerExit(Collider other) 
+    {
+        Door.GetComponent<Animator>().SetBool("open", false);
+    }
+}
